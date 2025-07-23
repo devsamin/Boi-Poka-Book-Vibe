c5b56221e9a4a063bc2dbdd1a6c13eaed3e7e7ac
devsamin <abdullahalsamin79@gmail.com>
2025-07-22 22:10:17 -0700

    nested route and navbar set up

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,25 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
+import Errorpage from './components/Errorpage/Errorpage.jsx';
+import Home from './components/Home/Home.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <Errorpage></Errorpage>,
+    children : [
+      {
+        path : "/",
+        element : <Home></Home>
+      },
+      {
+        path : "/dashboard",
+        element : <Dashboard></Dashboard>
+      }
+    ]
   },
 ]);
 
